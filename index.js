06ccfc95373771ca4de889f76d9b8e244f77ec95
sharon caroline <you@example.com>
2025-07-14 15:32:23 +0300

    Complete OOP lab with all classes and tests passing

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code here
+// Class for Breakfast
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+// Class for Lunch
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+// Class for Dinner
+class Dinner {
+  // Declare the private field for dessert
+  #dessert;
+
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.#dessert = dessert;
+  }
+
+  // Optional: Accessor method for dessert
+  getDessert() {
+    return this.#dessert;
+  }
+}
